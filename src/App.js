0844d3828f56b74c9ffdb6e3942c6b2e013679a4
adamjorycromer <adamjorycromer@gmail.com>
2019-03-30 12:06:34 -0600

    working on components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Nav from "./components/Nav";
+import Body from "./components/Body";
+import Card from "./components/Card";
+// import images from "./images";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="container">
+        <Nav />
+        <Body>
+
+        </Body>
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Nav(props) {
+    return (
+        <div>
+            <h1 className="title">Nav title</h1>
+        </div>
+    );
+}
+
+export default Nav;
+
